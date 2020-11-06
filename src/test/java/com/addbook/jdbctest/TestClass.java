a19c04d8d170150211bdc530116ebeb927f8d4ff
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-06 22:08:11 +0530

    Insert Into Table

--- src/test/java/com/addbook/jdbctest/TestClass.java
+++ src/test/java/com/addbook/jdbctest/TestClass.java
@@ -1,4 +1,46 @@
 package com.addbook.jdbctest;
 
+import com.addbook.jdbc.DBConnection;
+import com.addbook.jdbc.Details;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class TestClass {
+
+    DBConnection dbo;
+    @Before
+    public void init(){
+        dbo = new DBConnection();
+    }
+    @Test
+    public void check_retrieveData(){
+        List<Details> list = dbo.retrieveDataFromDB();
+//        System.out.println(list);
+        Assert.assertEquals(6, list.size());
+    }
+
+    @Test
+    public void Check_DataShouldGetUpdated() {
+        dbo.updateDetailsInDB("Bibhav",  "Teacher");
+
+
+    }
+
+    @Test
+    public void ShouldReturnDataByDate() {
+
+        List<Details> list1 = dbo.retrieveContactWithinDateRange("2017-01-01", "2019-12-31");
+        Assert.assertEquals(2, list1.size());
+    }
+
+    @Test
+    public void Check_DataShouldGetInsertedInTables() throws SQLException {
+
+        dbo.addContactAtomicTransaction("1", "Anushka", "Singh", "545214521", "anushka@nitt.edu", "Indira Nagar", "Lucknow",
+                                "Uttar Pradesh", "254145", "Friend", "Bibhav");
+    }
 }
