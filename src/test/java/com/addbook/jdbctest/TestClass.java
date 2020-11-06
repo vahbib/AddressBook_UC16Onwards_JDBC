3a4bf288d722fd3c1502322413d3ef3d00f9c72e
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-06 11:34:23 +0530

    Update AddressBook

--- src/test/java/com/addbook/jdbctest/TestClass.java
+++ src/test/java/com/addbook/jdbctest/TestClass.java
@@ -1,4 +1,30 @@
+
 package com.addbook.jdbctest;
 
+import com.addbook.jdbc.DBConnection;
+import com.addbook.jdbc.Details;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
 public class TestClass {
+    DBConnection dbo;
+    @Before
+    public void init(){
+        dbo = new DBConnection();
+    }
+    @Test
+    public void check_retrieveData(){
+        List<Details> list = dbo.retrieveDataFromDB();
+//        System.out.println(list);
+        Assert.assertEquals(6, list.size());
+    }
+
+    @Test
+    public void Check_DataShouldGetUpdated() {
+       dbo.updateDetailsInDB("Bibhav",  "Teacher");
+
+    }
 }
