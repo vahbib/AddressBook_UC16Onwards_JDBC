76754eda5b1814637fd859f3922f499a49decdb7
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-06 10:23:26 +0530

    Retrieve data from AddressBook Database

--- src/main/java/com/addbook/jdbc/DBConnection.java
+++ src/main/java/com/addbook/jdbc/DBConnection.java
@@ -1,13 +1,16 @@
 package com.addbook.jdbc;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 
 public class DBConnection {
+
+    static DBConnection ab_dbo = new DBConnection();
+
     public Connection getConnection(){
-        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String jdbcURL = "jdbc:mysql://localhost:3306/addressbookdb?useSSL=false";
         String userName = "root";
         String password = "Asep@25355";
 
@@ -35,5 +38,32 @@ public class DBConnection {
             Driver driverClass = (Driver) driverList.nextElement();
             System.out.println( "  " + driverClass.getClass().getName());
         }
+    }
+
+    public List<Details> retrieveDataFromDB(){
+        List<Details> contactList = new ArrayList<>();
+        try(Connection con = ab_dbo.getConnection()){
+            String query = "Select * from addressbook";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()){
+                String fname = rs.getString(1);
+                String lname = rs.getString(2);
+                long phone = rs.getLong(3);
+                String mail = rs.getString(4);
+                String address = rs.getString(5);
+                String city = rs.getString(6);
+                String state = rs.getString(7);
+                long zipcode = rs.getLong(8);
+                String type = rs.getString(9);
+                String personName = rs.getString(10);
+
+                Details details = new Details(fname,lname,phone,mail, address,city,state,zipcode,type, personName);
+                contactList.add(details);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return contactList;
     }
 }
