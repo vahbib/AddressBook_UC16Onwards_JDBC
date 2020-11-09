a90161e6c4b5be5adc83a644384888003172c1d7
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-09 10:58:32 +0530

    Added Multiple data using thread

--- src/main/java/com/addbook/jdbc/Details.java
+++ src/main/java/com/addbook/jdbc/Details.java
@@ -1,6 +1,23 @@
 package com.addbook.jdbc;
 
 public class Details {
+
+
+    public Details(int id, String firstName, String lastName, long phoneNumber, String emailId, String address, String city, String state, long zipCode, String type, String personName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailId = emailId;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.type = type;
+        this.personName = personName;
+    }
+
+    private int id;
     private String firstName;
     private String lastName;
     private long phoneNumber;
@@ -9,6 +26,57 @@ public class Details {
     private String city;
     private String state;
     private long zipCode;
+    private String type;
+    private String personName;
+    private String date;
+
+
+    public Details(int id, String firstName, String lastName, long phoneNumber,
+                   String emailId, String address, String city, String state, long zipCode, String type, String personName, String date) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailId = emailId;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.type = type;
+        this.personName = personName;
+        this.date = date;
+    }
+
+    public Details(String firstName, String lastName, long phoneNumber, String emailId, String date) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailId = emailId;
+        this.date = date;
+    }
+
+    public Details(String firstName, String lastName, long phoneNumber, String emailId, String address, String city, String state, long zipCode, String type, String personName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailId = emailId;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.personName = personName;
+        this.type = type;
+    }
+    public Details(String firstName, String lastName, long phoneNumber, String emailId, String address, String state, long zipCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailId = emailId;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.address = address;
+    }
+
 
     public Details(String firstName, String lastName, long phoneNumber, String emailId, String address, String city,
                    String state, long zipCode) {
@@ -24,6 +92,36 @@ public class Details {
     }
     public Details() {
         super();
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getCity() {
